9b9830369e6be490f4afe396b2a11dd6054e3575
Emman Nipal <emnnipal@gmail.com>
2023-03-26 16:43:40 +0800

    feat: add install script for lsp and formatters

---
+++ install.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+# install typescript language server and prettier as formatter
+pnpm install -g typescript typescript-language-server prettier
+
+# install deno and use as formatter
+# brew install deno
+
