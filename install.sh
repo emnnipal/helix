1e53593bce1dbd5040fb871fc04827914d6a29a5
Emman Nipal <emnnipal@gmail.com>
2023-03-29 00:16:11 +0800

    chore: install deno and add notes on using deno as formatter

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 
 # install helix
-brew install helix
+# install deno as formatter
+brew install \
+  helix \
+  deno \
 
 # install typescript language server and prettier as formatter
 pnpm install -g typescript typescript-language-server prettier
-
-# install deno and use as formatter
-# brew install deno
 
