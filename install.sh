7643f1bdebbd7a582dc307139e06d166a6191937
Emman Nipal <emnnipal@gmail.com>
2024-02-22 12:33:20 +0800

    chore: sanitize install script

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 # !/bin/bash
 
 # install helix
-# install deno as formatter
 brew install \
 	helix \
 	lf \
 
-# install typescript language server and prettier as formatter
+# install LSP related packages
 pnpm install -g \
 	typescript \
 	typescript-language-server \
 	prettier \
 	@tailwindcss/language-server \
 	@olrtg/emmet-language-server \
-  vscode-langservers-extracted \
+	vscode-langservers-extracted \
