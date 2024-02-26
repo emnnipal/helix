ee83b8d21c5092ba935949f833688e61a095b14c
Emman Nipal <emnnipal@gmail.com>
2024-02-26 19:55:22 +0800

    chore: add eslint in install script

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ pnpm install -g \
 	typescript \
 	typescript-language-server \
 	prettier \
+	eslint \
 	@tailwindcss/language-server \
 	@olrtg/emmet-language-server \
 	vscode-langservers-extracted \
