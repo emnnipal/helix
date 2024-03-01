8cc6489add49bfa8d9b40d607fcc083e1421b402
Emman Nipal <emnnipal@gmail.com>
2024-03-01 21:11:28 +0800

    chore: add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+
+# install helix
+# brew remove \
+# 	helix \
+# 	lf \
+
+# install LSP related packages
+pnpm remove -g \
+	typescript \
+	typescript-language-server \
+	prettier \
+	eslint \
+	@tailwindcss/language-server \
+	@olrtg/emmet-language-server \
+	vscode-langservers-extracted \
