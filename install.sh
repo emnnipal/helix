ad9c5ca42abd1523cccdb3f289eaf690141860a0
Emman Nipal <emnnipal@gmail.com>
2025-01-10 22:00:09 +0800

    chore: enhance install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
-# install helix
+# Install Helix and related packages
 brew install \
   helix \
   tree-sitter \
   lf
 
-# install LSP related packages
+# Install LSP-related packages
 pnpm install -g \
   typescript \
   typescript-language-server \
@@ -14,4 +14,10 @@ pnpm install -g \
   eslint \
   @tailwindcss/language-server \
   @olrtg/emmet-language-server \
-  vscode-langservers-extracted
+  vscode-langservers-extracted@4.8.0
+
+# TODO: Remove the version constraint for vscode-langservers-extracted (4.8.0) once the Helix ESLint issue in 4.10.0 is resolved.
+# References:
+#   https://github.com/hrsh7th/vscode-langservers-extracted/commit/859ca87fd778a862ee2c9f4c03017775208d033a#commitcomment-142613101
+#   https://github.com/hrsh7th/vscode-langservers-extracted/commit/859ca87fd778a862ee2c9f4c03017775208d033a#commitcomment-147447564
+#   https://www.reddit.com/r/HelixEditor/comments/1cr343d/eslint_lsp_does_not_work_has_anyone_got_it_working/
