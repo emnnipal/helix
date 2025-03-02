a17103675c7c5c7a7be282d72a3b40eb3b07e280
Emman Nipal <emnnipal@gmail.com>
2025-03-02 23:50:06 +0800

    feat: use tmux instead of zellij since tmux is a lot faster

--- picker.sh
+++ picker.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi_picker.sh open $(realpath $1 2>/dev/null)
+buffer_name="$(realpath "$2" 2>/dev/null)"
+paths=$(yazi $buffer_name --chooser-file=/dev/stdout)
+
+if [[ -n "$paths" ]]; then
+  tmux last-window
+  tmux send-keys Escape
+  tmux send-keys ":$1 $paths"
+  tmux send-keys Enter
+else
+  tmux kill-window -t fx
+fi
