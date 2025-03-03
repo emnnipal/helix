61ee301cf696d469bd08f24f4de05727db3d6281
Emman Nipal <emnnipal@gmail.com>
2025-03-03 18:14:42 +0800

    feat: open files in helix faster by pasting the command

--- s/picker.sh
+++ s/picker.sh
@@ -6,7 +6,14 @@ paths=$(yazi $buffer_name --chooser-file=/dev/stdout)
 if [[ -n "$paths" ]]; then
   tmux last-window
   tmux send-keys Escape
-  tmux send-keys ":open $paths"
+
+  # tmux send-keys -l ":open $paths"
+
+  # open files faster by running the paste command instead of typing the keys one by one
+  tmux set-buffer "open $paths"
+  tmux send-keys ":"
+  tmux paste-buffer -p
+
   tmux send-keys Enter
 else
   tmux kill-window -t fx
