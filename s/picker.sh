3acdea077a8b592cd51f516ae2011bd9566a5e5c
Emman Nipal <emnnipal@gmail.com>
2025-03-03 17:40:41 +0800

    chore: shorten file explorer command by removing open params

--- s/picker.sh
+++ s/picker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-buffer_name="$(readlink -f "$2" 2>/dev/null)"
+buffer_name="$(readlink -f "$1" 2>/dev/null)"
 paths=$(yazi $buffer_name --chooser-file=/dev/stdout)
 
 if [[ -n "$paths" ]]; then
   tmux last-window
   tmux send-keys Escape
-  tmux send-keys ":$1 $paths"
+  tmux send-keys ":open $paths"
   tmux send-keys Enter
 else
   tmux kill-window -t fx
