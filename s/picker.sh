84ba1754e4d16d937eef29f87965f24a08f2772a
Emman Nipal <emnnipal@gmail.com>
2025-03-04 19:44:24 +0800

    fix: use PWD if buffer doesn't exist

--- s/picker.sh
+++ s/picker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 buffer_name="$(readlink -f "$1" 2>/dev/null)"
+buffer_name="${buffer_name:-$PWD}" # Use PWD if readlink fails or returns empty
 paths=$(yazi "$buffer_name" --chooser-file=/dev/stdout | tr '\n' ' ')
 
 if [[ -n "$paths" ]]; then
